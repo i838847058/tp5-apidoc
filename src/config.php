d176422172a7b1018b04f0ccb01afe46fb0f4713
shuxiaoxian <winnie_xian@163.com>
2019-04-23 03:31:33 +0800

    xiaoyun

--- src/config.php
+++ src/config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'title' => "APi接口文档",  //文档title
+    'title' => "API接口文档",  //文档title
     'password' => '', //访问密码，为空不需要密码
     //静态资源路径--默认为云上路径，解决很多人nginx配置问题
     //可将assets目录拷贝到public下面，具体路径课自行配置
