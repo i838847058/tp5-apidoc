2623c1ea45b5c7f17ab70f8d5889c3dbe5aae108
shuxiaoxian <winnie_xian@163.com>
2020-03-20 15:43:34 +0800

    修复了json输出到调试器后格式的问题，已改为自动识别

--- src/helper.php
+++ src/helper.php
@@ -1,6 +1,6 @@
 <?php
-if(defined('THINK_VERSION')){
-    \think\Route::get('doc/assets', "\\Api\\Doc\\DocController@assets",['deny_ext'=>'php|.htacess']);
+if (defined('THINK_VERSION')) {
+    \think\Route::get('doc/assets', "\\Api\\Doc\\DocController@assets", ['deny_ext' => 'php|.htacess']);
     \think\Route::get('doc/search', "\\Api\\Doc\\DocController@search");
     \think\Route::get('doc/list', "\\Api\\Doc\\DocController@getList");
     \think\Route::get('doc/info', "\\Api\\Doc\\DocController@getInfo");
@@ -8,8 +8,8 @@ if(defined('THINK_VERSION')){
     \think\Route::any('doc/pass', "\\Api\\Doc\\DocController@pass");
     \think\Route::any('doc/login', "\\Api\\Doc\\DocController@login");
     \think\Route::get('doc', "\\Api\\Doc\\DocController@index");
-}else if (defined('THINK_VERSION') and THINK_VERSION  > 6){
-    Route::get('doc/assets', "\\Api\\Doc\\DocController@assets",['deny_ext'=>'php|.htacess']);
+} else {
+    Route::get('doc/assets', "\\Api\\Doc\\DocController@assets", ['deny_ext' => 'php|.htacess']);
     Route::get('doc/search', "\\Api\\Doc\\DocController@search");
     Route::get('doc/list', "\\Api\\Doc\\DocController@getList");
     Route::get('doc/info', "\\Api\\Doc\\DocController@getInfo");
@@ -29,25 +29,26 @@ if(defined('THINK_VERSION')){
  * @param array $headers
  * @return mixed
  */
-function http_request($url, $cookie, $data = array(), $method = array(), $headers = array()){
+function http_request($url, $cookie, $data = array(), $method = array(), $headers = array())
+{
     $curl = curl_init();
-    if(count($data) && $method == "GET"){
+    if (count($data) && $method == "GET") {
         $data = array_filter($data);
-        $url .= "?".http_build_query($data);
+        $url .= "?" . http_build_query($data);
         $url = str_replace(array('%5B0%5D'), array('[]'), $url);
     }
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
-    if (count($headers)){
+    if (count($headers)) {
         $head = array();
-        foreach ($headers as $name=>$value){
-            $head[] = $name.":".$value;
+        foreach ($headers as $name => $value) {
+            $head[] = $name . ":" . $value;
         }
         curl_setopt($curl, CURLOPT_HTTPHEADER, $head);
     }
     $method = strtoupper($method);
-    switch($method) {
+    switch ($method) {
         case 'GET':
             break;
         case 'POST':
@@ -62,7 +63,7 @@ function http_request($url, $cookie, $data = array(), $method = array(), $header
             curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
             break;
     }
-    if (!empty($cookie)){
+    if (!empty($cookie)) {
         curl_setopt($curl, CURLOPT_COOKIE, $cookie);
     }
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -74,10 +75,11 @@ function http_request($url, $cookie, $data = array(), $method = array(), $header
 /**
  * array_column
  */
-if(!function_exists("array_column"))
-{
-    function array_column($array,$column_name)
+if (!function_exists("array_column")) {
+    function array_column($array, $column_name)
     {
-        return array_map(function($element) use($column_name){return $element[$column_name];}, $array);
+        return array_map(function ($element) use ($column_name) {
+            return $element[$column_name];
+        }, $array);
     }
 }
